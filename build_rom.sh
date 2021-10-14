df2b153de2789de35c13da2cb29322e71e9fa229
harshaxvarthan <82403923+harshaxvarthan@users.noreply.github.com>
2021-10-15 03:06:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,14 +4,15 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j$(nproc --all)
+lunch xtended_vayu-userdebug
+export TZ=Asia/Dhaka #put before last build command
+make xtended
 
 # upload rom
 up(){
@@ -19,4 +20,4 @@ up(){
 	# 14 days, 10 GB limit
 }
 
-up out/target/product/mido/*.zip
+up out/target/product/vayu/*.zip
