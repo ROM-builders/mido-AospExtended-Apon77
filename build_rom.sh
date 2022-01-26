24ce53fd2687f09536aad18e24f7a346baa3011e
Azure-007 <64735082+Azure-007@users.noreply.github.com>
2022-01-26 17:03:42 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,14 +4,14 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-19.0
+git clone https://github.com/Azure-007/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j$(nproc --all)
+lunch lineage_raphael-userdebug
+make bacon -j$(nproc --all)
 
 # upload rom
 up(){
