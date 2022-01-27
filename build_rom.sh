2dee4a8dba7ff148372c90a49ad9e7aff43bca11
Azure-007 <64735082+Azure-007@users.noreply.github.com>
2022-01-27 15:01:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ set -x
 
 # sync rom
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1
-git clone https://github.com/Azure-007/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Azure-007/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
