ec9ec8b2c2797fc4cfc836de9efc4fd92a45816f
Yshmany <95184940+Yshmany@users.noreply.github.com>
2022-03-04 20:24:36 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,13 +4,13 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init -u https://github.com/LineageOS/android.git -b lineage-18.1
+git clone https://github.com/Yshmany/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
+lunch lineage_channel-user
 m aex -j$(nproc --all)
 
 # upload rom
