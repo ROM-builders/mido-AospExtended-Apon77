221f7a8d2e880e42feca20da1ebfe5c0d3ab7431
noone-64 <108020142+noone-64@users.noreply.github.com>
2022-07-23 00:20:08 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+sudo apt install unzip
