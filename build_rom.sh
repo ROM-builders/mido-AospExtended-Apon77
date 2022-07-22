e5a83430f80c37046404dad397294adfe887f03f
noone-64 <108020142+noone-64@users.noreply.github.com>
2022-07-23 00:14:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/Corvus-R/android_manifest -b 12-test
+git clone https://github.com/zeelog/android_device_xiaomi_mido --depth 1 -b lineage-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
